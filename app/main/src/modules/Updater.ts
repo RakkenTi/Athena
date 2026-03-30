d77b0066bc96142420cfc1a1fc0e5f71596c8540
RakkenTi <martin3zmrk@gmail.com>
2026-03-29 23:54:11 -0400

    added basic logging to updater

--- app/main/src/modules/Updater.ts
+++ app/main/src/modules/Updater.ts
@@ -1,6 +1,23 @@
+import { autoUpdater } from 'electron'
 import { updateElectronApp, UpdateSourceType } from 'update-electron-app'
 
 export const startAutoUpdater = () => {
+    autoUpdater.on('error', (error) => {
+        console.error('Update Error:', error)
+    })
+
+    autoUpdater.on('checking-for-update', () => {
+        console.log('Checking for update...')
+    })
+
+    autoUpdater.on('update-available', () => {
+        console.log('Update available!')
+    })
+
+    autoUpdater.on('update-not-available', () => {
+        console.log('Update not available. Current version:')
+    })
+
     updateElectronApp({
         updateSource: {
             type: UpdateSourceType.ElectronPublicUpdateService,
